9a494d19ebe1cfda0759b2588c0fd5cf972f37e8
Denis Ohrimenko <woxer77@gmail.com>
2026-02-01 16:33:15 +0200

    feat: implemented silly topbar sort component

--- src/modules/topbar/components/sort.tsx
+++ src/modules/topbar/components/sort.tsx
@@ -4,17 +4,38 @@ import { ArrowUpDown } from 'lucide-react';
 
 import type { ClassProps } from '@/types/global';
 import { cn } from '@/lib/utils';
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/ui/select';
 
 const Sort: React.FC<ClassProps> = ({ className }) => {
+  const TEMP_SORT = [
+    { value: 'popular', name: 'popular' },
+    { value: 'alphabet', name: 'alphabet' }
+  ];
+
   return (
-    <div
-      className={cn(
-        'flex cursor-pointer items-center gap-x-1 rounded-md bg-neutral-100 p-3.5 px-5 font-semibold',
-        className
-      )}>
-      <ArrowUpDown size={16} />
-      Sort by: <p className="text-red-700">popular</p>
-    </div>
+    <Select>
+      <SelectTrigger
+        className={cn(
+          'cursor-pointer gap-1 rounded-md border-none bg-neutral-100 px-5 py-2.5 text-base font-semibold lowercase shadow-none',
+          '[&_[data-slot=select-value]]:text-red-700',
+          'data-[placeholder]:text-foreground',
+          className
+        )}
+        iconClassName="hidden">
+        <ArrowUpDown size={16} />
+        <p>
+          <span className="capitalize">Sort</span> by:
+        </p>
+        <SelectValue placeholder="popular" />
+      </SelectTrigger>
+      <SelectContent position="popper" className="w-auto rounded-md lowercase">
+        {TEMP_SORT.map(({ value, name }) => (
+          <SelectItem key={value} value={value} className="rounded-sm">
+            {name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 };
 

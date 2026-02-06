ebd0b92ac2a19cd8e0dcdba5cb737190652664e6
Denis Ohrimenko <woxer77@gmail.com>
2026-02-06 15:25:00 +0200

    feat: temporarily removed suspense from homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,7 @@
-import { Suspense } from 'react';
-
 import { Header } from '@/modules/header/index';
 import { TopBar } from '@/modules/topbar/index';
 import { Filter } from '@/modules/filter/index';
 import { Products } from '@/modules/products/index';
-import { Skeleton } from '@/ui/skeleton';
 
 export default function Home() {
   return (
@@ -13,12 +10,8 @@ export default function Home() {
       <div className="h-[2000px]">
         <TopBar />
         <div className="container mt-10 flex h-full gap-18">
-          <Suspense fallback={<Skeleton />}>
-            <Filter />
-          </Suspense>
-          <Suspense fallback={<Skeleton />}>
-            <Products />
-          </Suspense>
+          <Filter />
+          <Products />
         </div>
       </div>
     </div>

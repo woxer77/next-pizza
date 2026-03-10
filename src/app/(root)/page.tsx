88ec645b2fe47312888da1a5da11b8f404c32bc5
Denis Ohrimenko <woxer77@gmail.com>
2026-03-10 17:56:13 +0200

    fix(vercel): wrapped Filter in Suspense

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -6,11 +6,14 @@ import ProductsSkeleton from '@/ui/products-skeleton';
 
 export const revalidate = 60; // TODO: FOR DEVELOPMENT ONLY
 export default function Home() {
+  // TODO: FILTER SUSPENSE - TEMPORAL
   return (
     <>
       <TopBar />
       <div className="container mt-10 flex h-full gap-18">
-        <Filter className="top-35" />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Filter className="top-35" />
+        </Suspense>
         <Suspense fallback={<ProductsSkeleton limit={6} />}>
           <Products />
         </Suspense>
